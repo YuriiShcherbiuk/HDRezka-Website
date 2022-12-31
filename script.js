9e5e4af8e070d87c745e2bdc74b8d0ec7a03a5c2
YuRaFurious <shcherbiuk.yurii.email@gmail.com>
2022-12-31 18:35:46 +0200

    1

--- script.js
+++ script.js
@@ -19,6 +19,39 @@ if (window.matchMedia) {
     document.body.className = theme;
 }
 
+//Font size
+
+const resizeObserver = new ResizeObserver((entries) => {
+    for (const entry of entries) {
+        const v = Math.min(entry.contentRect.width, entry.contentRect.height);
+        document.querySelector('.logo').style.fontSize = v * 0.12 + 'px';
+
+        document.querySelector('.logo svg').style.cssText = `
+        max-width: ${v * 0.15}px;
+        max-height: ${v * 0.15}px;
+        margin-left: ${v * 0.015}px;
+        padding: ${v * 0.015}px ${v * 0.015}px ${v * 0.015}px 0;
+        `;
+
+        document.querySelector('.info').style.fontSize = v * 0.06 + 'px';
+
+        document.querySelector('.telegram').style.cssText = `
+        font-size: ${v * 0.06}px;
+        padding: ${v * 0.0225}px ${v * 0.045}px;
+        border-radius: ${v * 0.03}px;
+        `;
+
+        document.querySelector('.telegram svg').style.cssText = `
+        max-width: ${v * 0.09}px;
+        max-height: ${v * 0.09}px;
+        margin-left: ${v * 0.015}px;
+        padding: ${v * 0.015}px ${v * 0.015}px ${v * 0.015}px 0;
+        `;
+    }
+});
+
+resizeObserver.observe(document.querySelector('.sex'));
+
 //Snowfall
 
 class Perlin {
